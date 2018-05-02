f6cd4cd3ccdfc8898a286e66c37357ac56a270f2
Krivonosov Mikhail <mike_live@mail.ru>
2018-05-02 19:09:32 +0300

    Make travis. Init model.

---
+++ code/ScoreBoard/Model/src/test/java/ru/butterfly/ScoreBoard/Model/ScoreBoardTests.java
@@ -0,0 +1,15 @@
+package ru.butterfly.ScoreBoard.Model;
+
+import org.junit.Test;
+
+//import static org.junit.Assert.assertEquals;
+//import static org.junit.Assert.assertNotNull;
+//import static org.junit.Assert.assertTrue;
+//import static org.junit.Assert.assertFalse;
+
+public class ScoreBoardTests {
+    @Test
+    public void canCreateScoreBoard() {
+        ScoreBoard board = new ScoreBoard();
+    }
+}
